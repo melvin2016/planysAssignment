1c1b0e5c0cd79f74478e55089719eb013faaa072
Melvin <melvingeorge10@gmail.com>
2018-11-24 10:14:07 +0530

    added Procfile

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express'); 
 const app = express();
-
+const PORT = process.env.PORT || 3000;
 app.use(express.static('static'));
 
-app.listen('3000',()=>{
-    console.log('Server running on PORT 3000');
+app.listen(PORT,()=>{
+    console.log(`Server running on PORT : ${PORT}`);
 })
